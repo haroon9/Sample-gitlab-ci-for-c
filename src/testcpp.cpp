d011582ac29cc7331970bb2447fb94f412e6ef78
Haroon Gul <haroon.gul9@gmail.com>
2019-09-19 13:52:41 +0000

    Update testcpp.cpp

--- src/testcpp.cpp
+++ src/testcpp.cpp
@@ -1,8 +1,7 @@
-	#include<iostream>
-	using namespace std;
-
-	int main()
-	{
-		cout << "Hello World! from c++";
-		return 0;
-	}
+#include <iostream>
+ 
+int main()
+{
+  std::cout << "Hello World! C++" << std::endl;
+  return 0;
+}
